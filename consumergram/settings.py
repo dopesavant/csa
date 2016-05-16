813996c1ee0c30809aae7b226eddce991b6bb950
dopesavant <f.inetimi@outlook.com>
2016-05-16 17:23:34 -0500

    whitenoise

--- consumergram/settings.py
+++ consumergram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$w)^y*sk(z6he7o0)stw#8#y9^rhyyfg)(-s3%m*^18jl96j(e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
