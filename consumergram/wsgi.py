e2bf36517fb46e14de884cc2fc235dabbd875fe1
dopesavant <f.inetimi@outlook.com>
2016-05-16 18:21:08 -0500

    whitenoise

--- consumergram/wsgi.py
+++ consumergram/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "consumergram.settings")
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "consumergram.settings")
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
